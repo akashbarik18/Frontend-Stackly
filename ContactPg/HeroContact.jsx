d390e10e6a5a6c0b147a758e6f83eedd7a3993de
akashbarik18 <ab361271@gmail.com>
2025-07-04 13:49:38 +0530

    Add files via upload

---
+++ ContactPg/HeroContact.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import ContactBanner from './ContactBanner'
+import ContactForm from './ContactForm'
+import Profile from '../Profile/Profile'
+
+export default function HeroContact() {
+  return (
+    <div>
+      
+      <ContactBanner/>
+
+      <ContactForm/>
+
+      {/* <Profile /> */}
+    </div>
+  )
+}
